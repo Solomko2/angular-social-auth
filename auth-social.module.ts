39e998ff91562459c7cd966d52a1dc32c5809227
Dani <bidstopper@gmail.com>
2019-01-06 21:05:26 +0100

    Fixed library to work with angular7

--- auth-social.module.ts
+++ auth-social.module.ts
@@ -1,7 +1,8 @@
-import {NgModule} from '@angular/core';
-import {SocialService} from './social.service';
-import {HttpClientModule} from '@angular/common/http';
-import {RouterModule} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SocialService } from './social.service';
+
 import * as fromComponents from './components';
 
 @NgModule({
